1cf12870e18c6daff3a5676634ddea97be488dcb
jerryphm <jerrypham.work@gmail.com>
2025-02-14 17:25:49 +0700

    feat: setup Hilt for automated DI

---
+++ app/src/main/java/com/example/chitchat/ChitChatApplication.kt
@@ -0,0 +1,9 @@
+package com.example.chitchat
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class ChitChatApplication: Application() {
+
+}
--- app/src/main/java/com/example/chitchat/MainActivity.kt
+++ app/src/main/java/com/example/chitchat/MainActivity.kt
@@ -12,6 +12,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.chitchat.ui.theme.ChitChatTheme
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
